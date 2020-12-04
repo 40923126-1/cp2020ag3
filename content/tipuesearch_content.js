c8ada66ab699d5d4f45bf651a0b0e9965c70abfe
unknown <40923126@gm.nfu.edu.tw>
2020-12-04 11:23:34 +0800

    cp2020ag3-hw1

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'This is  https://github.com/mdecourse/cmstemplate \n \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'This is  https://github.com/mdecourse/cmstemplate \n \n', 'tags': '', 'url': 'About.html'}, {'title': 'HW1作業', 'text': '', 'tags': '', 'url': 'HW1作業.html'}]};
